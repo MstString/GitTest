8409b130bd722fca07ca2642818e40d78c46a9b0
LEEZY <im_lzy@126.com>
2019-12-17 23:01:35 +0800

    ddd

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Test Test And Test 666!");
         System.out.println("Hello DEV");
+        System.out.println("Hello DEV DEV");
     }
 }
